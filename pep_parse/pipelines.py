0e8448330e4183c5390d9b0f93f0a6311ae43301
tWoAlex <thewoalex@gmail.com>
2023-09-08 17:33:15 +0500

    Third review

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -10,20 +10,20 @@ class PepParsePipeline:
         self.status_counters = Counter()
 
     def process_item(self, item, spider):
-        self.status_counters += {item['status']: 1}
+        self.status_counters[item['status']] += 1
         return item
 
     def close_spider(self, spider):
         results_dir = BASE_DIR.joinpath(RESULTS_DIR)
-        results_dir.mkdir(exist_ok=True)
-
         timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H-%M-%S')
         output_file = results_dir.joinpath(f'status_summary_{timestamp}.csv')
-
         with open(output_file, 'w') as file:
-            lines = [('status', 'count')]
-            lines.extend(sorted(list(self.status_counters.items())))
-            lines.extend([('Total', sum(self.status_counters.values()))])
+            lines = [
+                ('status', 'count'),
+                *sorted(list(self.status_counters.items())),
+                ('Total', sum(self.status_counters.values())),
+            ]
 
-            writer = csv.writer(file, dialect='unix', quoting=csv.QUOTE_NONE)
+            writer = csv.writer(file, dialect=csv.unix_dialect(),
+                                quoting=csv.QUOTE_NONE)
             writer.writerows(lines)
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -14,17 +14,15 @@ class PepSpider(scrapy.Spider):
     start_urls = [f'https://{domain}/' for domain in allowed_domains]
 
     def parse(self, response):
-        pep_links = response.css(
-            'section[id=numerical-index] tbody a::attr(href)').getall()
-
-        for link in pep_links:
+        for link in response.css(
+            'section[id=numerical-index] tbody a::attr(href)'
+        ).getall():
             yield response.follow(link, callback=self.parse_pep)
 
     def parse_pep(self, response):
         title = response.css('h1.page-title::text').get()
         number, name = re.search(PEP_TITLE_REGEX, title).groups()
         status = response.css('dl.field-list abbr::text').get()
-
         yield PepParseItem(number=number,
                            name=name,
                            status=status)
