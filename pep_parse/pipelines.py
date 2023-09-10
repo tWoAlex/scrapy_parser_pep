eb88b28601d9ff27e7ff7fa3a46935e3ff452311
tWoAlex <thewoalex@gmail.com>
2023-09-10 13:26:54 +0500

    Sixth review

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -6,6 +6,10 @@ from pep_parse.settings import BASE_DIR, RESULTS_DIR
 
 
 class PepParsePipeline:
+    def __init__(self):
+        self.results_dir = BASE_DIR.joinpath(RESULTS_DIR)
+        self.results_dir.mkdir(exist_ok=True)
+
     def open_spider(self, spider):
         self.status_counters = Counter()
 
@@ -14,17 +18,16 @@ class PepParsePipeline:
         return item
 
     def close_spider(self, spider):
-        results_dir = BASE_DIR.joinpath(RESULTS_DIR)
-        results_dir.mkdir(exist_ok=True)
         timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H-%M-%S')
-        output_file = results_dir.joinpath(f'status_summary_{timestamp}.csv')
+        output_file = self.results_dir.joinpath(
+            f'status_summary_{timestamp}.csv')
         with open(output_file, 'w') as file:
             writer = csv.writer(file, dialect=csv.unix_dialect(),
                                 quoting=csv.QUOTE_NONE)
             writer.writerows(
                 [
                     ('status', 'count'),
-                    *sorted(list(self.status_counters.items())),
+                    *sorted(self.status_counters.items()),
                     ('Total', sum(self.status_counters.values())),
                 ]
             )
